866e1cebc807dc510059e52b4eeaa93599f3d582
techhack01 <raj.roshancs103d02@gmail.com>
2025-12-25 02:54:15 -0500

    updating eks cluster version

--- aws-eks/variables.tf
+++ aws-eks/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.31"
 }
 
 variable "vpc_cidr" {
