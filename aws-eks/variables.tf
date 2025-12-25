1f94b7ec6c7d68881daa010719c7f2df8522d8b3
Techack01 <raj.roshancs103d02@gmail.com>
2025-12-25 01:42:06 -0500

    Adding tf files to create eks cluster (#7)
    * adding sample tf files
    
    * testing
    
    * testing
    
    * adding sample tf files
    
    * adding a folder
    
    * deleting file
    
    * delete
    
    * remove tags
    
    * update region
    
    * adding tf files for eks creation
    
    * adding tf code
    
    * test change

---
+++ aws-eks/variables.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "workstation_cidr" {
+  description = "CIDR block for workstation access"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "capacity_type" {
+  description = "Type of capacity associated with the EKS Node Group. Valid values: ON_DEMAND, SPOT"
+  type        = string
+  default     = "ON_DEMAND"
+}
+
+variable "instance_types" {
+  description = "List of instance types associated with the EKS Node Group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_capacity" {
+  description = "Desired number of nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_capacity" {
+  description = "Maximum number of nodes"
+  type        = number
+  default     = 4
+}
+
+variable "min_capacity" {
+  description = "Minimum number of nodes"
+  type        = number
+  default     = 1
+}
